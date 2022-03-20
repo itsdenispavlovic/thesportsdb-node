4e05a10a1a76fdac9bfacb586ca990917a604900
Denis Pavlovic <contact@denispavlovic.com>
2022-03-20 22:38:53 +0100

    Get Team Details

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import {myApp, PORT} from "./utils/client.js";
+import cors from 'cors';
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
 
 const app = express();
+
+app.use(cors(corsOptions));
+
 
 app.listen(PORT, () => {
     console.log(`Running this app on the port ${PORT}.`);
@@ -12,10 +21,15 @@ app.get('/get-last-events', (req, res) => {
 
     myApp
         .get(`/eventspastleague.php?id=${ENG_PREMIER_LEAGUE_ID}`)
-        .then(response => {
-            res.send({
-                data: response.data
-            });
-        })
+        .then(response => res.send(response.data))
         .catch(e => res.status(400).send({ error: e }));
 });
+
+app.post('/get-team-details', express.json(), (req, res) => {
+    const { teamId } = req.body;
+
+    myApp
+        .get(`/lookupteam.php?id=${teamId}`)
+        .then(response => res.send(response.data))
+        .catch(e => res.status(400).send({ error: e }));
+})
